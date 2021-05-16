47493e52ef86fa930046d2e57f80303b5d8261bc
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-05-15 22:54:03 -0700

    connecting to mongodb completed

--- pages/api/vehicles/index.js
+++ pages/api/vehicles/index.js
@@ -1,7 +1,10 @@
 import nc from 'next-connect';
+import dbConnect from '../../../config/dbConnect';
 import { allVehicles } from '../../../controllers/vehicleControllers';
 
 const handler = nc();
+dbConnect();
+
 
 handler.get(allVehicles);
 
