a046bb4f928599ea0133ee2afee381569ff50ad5
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-05-17 21:43:14 -0700

    fetch all vehicles implemented

--- controllers/vehicleControllers.js
+++ controllers/vehicleControllers.js
@@ -1,10 +1,21 @@
 import Vehicle from '../models/vehicle';
 
-const allVehicles = (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: 'All Vehicles'
-    });
+// fetch all vehicles   =>   /api/rooms
+const allVehicles = async (req, res) => {
+    try {
+        const vehicles = await Vehicle.find();
+        res.status(200).json({
+            success: true,
+            count: vehicles.length,
+            vehicles
+        })
+    } catch(err) {
+        res.status(400).json({
+            success: false,
+            error: error
+        })
+    }
+
 }
 // Create new vehicle   =>   /api/vehicles
 const newVehicle = async (req, res) => {
