1b100302f03d2a273b6a79f8e6bf2e48173ac6de
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-06-16 23:48:01 -0700

    register user component implemented

--- redux/reducers/reducers.js
+++ redux/reducers/reducers.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
 import { allVehiclesReducer, vehicleDetailsReducer } from './vehicleReducers';
+import { authReducer } from './userReducers';
 
 
 const reducer = combineReducers({
     allVehicles: allVehiclesReducer,
-    vehicleDetails: vehicleDetailsReducer
+    vehicleDetails: vehicleDetailsReducer,
+    auth: authReducer,
+
 })
 
 export default reducer 
