c9eab128ac88dd8e40076cfb93659f81ce62ba31
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-06-20 18:53:18 -0700

    load logged in user implemented

--- redux/actions/userActions.js
+++ redux/actions/userActions.js
@@ -4,6 +4,9 @@ import {
     REGISTER_USER_REQUEST,
     REGISTER_USER_SUCCESS,
     REGISTER_USER_FAIL,
+    LOAD_USER_REQUEST,
+    LOAD_USER_SUCCESS,
+    LOAD_USER_FAIL,
 
     CLEAR_ERRORS
 
@@ -35,7 +38,27 @@ export const registerUser = (userData) => async (dispatch) => {
         })
     }
 }
+// Loader user
+export const loadUser = () => async (dispatch) => {
+    try {
 
+        dispatch({ type: LOAD_USER_REQUEST });
+
+        const { data } = await axios.get('/api/me')
+
+        dispatch({
+            type: LOAD_USER_SUCCESS,
+            payload: data.user
+        })
+
+    } catch (error) {
+
+        dispatch({
+            type: LOAD_USER_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
 // Clear Errors
 export const clearErrors = () => async (dispatch) => {
     dispatch({
--- redux/reducers/userReducers.js
+++ redux/reducers/userReducers.js
@@ -2,7 +2,9 @@ import {
     REGISTER_USER_REQUEST,
     REGISTER_USER_SUCCESS,
     REGISTER_USER_FAIL,
-
+    LOAD_USER_REQUEST,
+    LOAD_USER_SUCCESS,
+    LOAD_USER_FAIL,
     CLEAR_ERRORS
 
 } from '../constants/userConstants'
@@ -39,3 +41,38 @@ export const authReducer = (state = { user: null }, action) => {
             return state
     }
 }
+// Load user reducer
+export const loadedUserReducer = (state = { loading: true, user: null }, action) => {
+    switch (action.type) {
+
+        case LOAD_USER_REQUEST:
+            return {
+                loading: true,
+                isAuthenticated: false
+            }
+
+        case LOAD_USER_SUCCESS:
+            return {
+                loading: false,
+                isAuthenticated: true,
+                user: action.payload
+            }
+
+        case LOAD_USER_FAIL:
+            return {
+                loading: false,
+                isAuthenticated: false,
+                error: action.payload
+            }
+
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null
+            }
+
+        default:
+            return state
+    }
+}
+
