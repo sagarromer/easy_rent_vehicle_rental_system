93b1d3e06f6919dd28d084c396e51a361bbb33dd
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-06-25 22:15:35 -0700

    send password recovery email implemented with mailtrap

--- next.config.js
+++ next.config.js
@@ -6,6 +6,13 @@ module.exports = {
     CLOUDINARY_CLOUD_NAME: 'sega-soft',
     CLOUDINARY_API_KEY: '',
     CLOUDINARY_API_SECRET: '',
+    
+    SMTP_HOST: "",
+    SMTP_PORT: "",
+    SMTP_USER: "",
+    SMTP_PASSWORD: "",
+    SMTP_FROM_EMAIL: "",
+    SMTP_FROM_NAME: "",
 },
 images: {
   domains: ['res.cloudinary.com'],
