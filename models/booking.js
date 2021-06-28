a46b3a7f973fd167e627428d80cb5c44bf334c1c
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-06-27 22:28:14 -0700

    vehical booking model created

---
+++ models/booking.js
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose';
+//import timeZone from 'mongoose-timezone'
+
+const bookingSchema = new mongoose.Schema({
+    vehicle: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Vehicle'
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    endDate: {
+        type: Date,
+        required: true,
+    },
+    startDate: {
+        type: Date,
+        required: true,
+    },
+    amountPaid: {
+        type: Number,
+        required: true,
+    },
+    daysOfStay: {
+        type: Number,
+        required: true,
+    },
+    paymentInfo: {
+        id: {
+            type: String,
+            required: true,
+        },
+        status: {
+            type: String,
+            required: true,
+        }
+    },
+    paidAt: {
+        type: Date,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+//bookingSchema.plugin(timeZone);
+
+export default mongoose.models.Booking || mongoose.model('Booking', bookingSchema) 
